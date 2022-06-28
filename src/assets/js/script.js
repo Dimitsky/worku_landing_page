f72db02ada7d44107fa9b289510924e7e1f9f8de
Evmenov Dmitry <evmenovdk@outlook.com>
2022-06-28 18:09:12 +0400

    Подогнал плеер youtube под размер контейнера

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,8 +1,10 @@
 
 
+// Заменяет заглушку на плеер youtube
 function createIframe( div ) {
     let iframe = document.createElement( 'iframe' );
 
+    iframe.classList.add( 'video-player__iframe' );
     iframe.setAttribute( 'src', 'https://www.youtube.com/embed/' + div.dataset.id + '?autoplay=1&rel=0' );
     iframe.setAttribute('frameborder', '0');
     iframe.setAttribute('allowfullscreen', '1');
@@ -10,17 +12,18 @@ function createIframe( div ) {
     div.parentNode.replaceChild(iframe, div);
 }
 
+// Добавляет заглушку для плеера youtube
 function initYouTubeVideo( playerEl ) {
-    // 
+    // Получаем id ролика, ссылку на превью и создаем контейнер
     let id = playerEl.dataset.id;
     let thumbnail = playerEl.dataset.thumbnail;
     let divNode = document.createElement( 'div' );
 
-    divNode.classList.add( 'video-player__container' );
+    divNode.classList.add( 'video-player' );
     divNode.setAttribute( 'data-id', id );
     divNode.style.backgroundImage = 'url(\'THUMBNAIL\')'.replace( 'THUMBNAIL', thumbnail );
 
-    // 
+    // Создаем иконку для кнопки play
     let svgNode = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     let useNode = document.createElementNS("http://www.w3.org/2000/svg", "use");
 
@@ -29,18 +32,17 @@ function initYouTubeVideo( playerEl ) {
     useNode.setAttribute( 'href', 'assets/img/defs.svg#play' );
     svgNode.appendChild( useNode );
 
-    // 
+    // Кнопка плеер
     let playBtn = document.createElement( 'button' );
 
     playBtn.className = 'reset-button video-player__play-button';
+    // При нажатии на кнопку play - заменить заглушку на плеер youtube
     playBtn.addEventListener( 'click', function() {
-        createIframe( playerEl );
+        createIframe( divNode );
     } );
     playBtn.appendChild( svgNode );
     divNode.appendChild( playBtn );
-
-    // 
-    playerEl.appendChild( divNode );
+    playerEl.parentNode.replaceChild( divNode, playerEl );
 }
 
 initYouTubeVideo( document.querySelector( '.video-player' ) );
